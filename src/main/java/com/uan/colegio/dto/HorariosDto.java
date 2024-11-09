7cec0f5d224893ada9764baa20d116ab577ed97b
Leonardo Ramirez <48809204+leoram7205@users.noreply.github.com>
2024-11-09 15:16:48 -0500

    Ajustes fechas en hirarios

--- src/main/java/com/uan/colegio/dto/HorariosDto.java
+++ src/main/java/com/uan/colegio/dto/HorariosDto.java
@@ -4,6 +4,10 @@ import java.time.LocalTime;
 import java.util.Date;
 import java.util.UUID;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +20,9 @@ public class HorariosDto {
 	private ColegiosDto colegiosDto;
 	private String hoCodigo;
 	private String hoDesc;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date hoFechaIni;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date hoFechaFin;
 	private LocalTime hoHoraIni;
 	private LocalTime hoHoraFin;
--- src/main/java/com/uan/colegio/entity/Horarios.java
+++ src/main/java/com/uan/colegio/entity/Horarios.java
@@ -5,12 +5,16 @@ import java.time.LocalTime;
 import java.util.Date;
 import java.util.UUID;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,7 +33,11 @@ public class Horarios {
 	Colegios colegios;
 	private String hoCodigo;
 	private String hoDesc;
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date hoFechaIni;
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date hoFechaFin;
 	private LocalTime hoHoraIni;
 	private LocalTime hoHoraFin;
